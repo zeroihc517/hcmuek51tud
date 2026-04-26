dc3eb6e07ce2a80a4f8a6ca69a16ceac4fb97298
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-04-26 09:12:29 +0700

    Update database.js
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- HK2/database.js
+++ HK2/database.js
@@ -517,5 +517,13 @@ var DATA_DEADLINES = [
         icon: "fire",
         emoji: "📣",
         weeks: [9]
-    }
+    },
+
+];
+var DATA_LOCATIONS = [
+    { name: "Cơ sở 1: An Dương Vương (280 An Dương Vương, phường Chợ Quán, TP.HCM)", color: "#e0f2fe", link: "https://maps.app.goo.gl/Q8dRKtTZcqGeuEmy5" },
+    { name: "Cơ sở 2: Lê Văn Sỹ (222 Lê Văn Sỹ, phường Nhiêu Lộc, TP.HCM)", color: "#e6f9ef", link: "https://maps.app.goo.gl/7zCgiMmscdPFfCFv5" },
+    { name: "Cơ sở KTX: Lạc Long Quân (351A Lạc Long Quân, phường Hòa Bình, TP.HCM)", color: "#fff3e0", link: "https://maps.app.goo.gl/oV1mXHYDuW44cGbN6" },
+    { name: "Công viên Lê Thị Riêng (875 Cách Mạng Tháng Tám, phường Hòa Hưng, TP.HCM)", color: "#fef9c3", link: "https://maps.app.goo.gl/K7GzwaEcJwSb9dwGA" },
+    { name: "Học online (Hệ thống VLE; Phòng học ảo TEAM, Google Meet, Zoom)", color: "#f3e8ff", link: "#" }
 ];
