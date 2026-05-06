d708089a1bc6e3ce7d446ab6e71359bd72b8776f
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-05-06 08:05:10 +0700

    Update database.js
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- HK2/database.js
+++ HK2/database.js
@@ -375,7 +375,7 @@ name: `
 },
     // Ví dụ: Thi môn Giải tích vào Ca 1 (Sáng) Thứ 2 tuần 18
 {
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Lập trình nâng cao", // Nhập thẳng tên môn
+    name: "Lập trình nâng cao", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "Đang cập nhật", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "9:30 - 11:30",
@@ -384,7 +384,7 @@ name: `
     weeks: [17]
 },
 {
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Giải tích hàm nhiều biến", // Nhập thẳng tên môn
+    name: "Giải tích hàm nhiều biến", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "B.211", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "13:00 - 14:30",
@@ -393,7 +393,7 @@ name: `
     weeks: [16]
 },
 	{
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Chủ nghĩa xã hội khoa học", // Nhập thẳng tên môn
+    name: "Chủ nghĩa xã hội khoa học", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "B.112", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "9:30 - 10:30",
@@ -402,7 +402,7 @@ name: `
     weeks: [16]
 },
 		{
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Kinh tế chính trị học Mác-Lênin", // Nhập thẳng tên môn
+    name: "Kinh tế chính trị học Mác-Lênin", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "B.111", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "7:00 - 08:00",
@@ -411,7 +411,7 @@ name: `
     weeks: [16]
 },
 		{
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Hình học cao cấp hai chiều và ba chiều", // Nhập thẳng tên môn
+    name: "Hình học cao cấp hai chiều và ba chiều", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "B.117", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "7:00 - 08:30",
@@ -420,7 +420,7 @@ name: `
     weeks: [18]
 },
 			{
-    name: "<b>Kiểm tra Kết thúc học phần</b><br>Không gian tuyến tính", // Nhập thẳng tên môn
+    name: "Không gian tuyến tính", // Nhập thẳng tên môn
     location: "Cơ sở: An Dương Vương",
     room: "B.117", link: "https://maps.app.goo.gl/c9JKtuZmpjoGx24M9",
     time: "7:00 - 08:30",
