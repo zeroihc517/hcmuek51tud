40b533ce136e762b09a6d0b21b0721a94046d418
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-06-17 07:39:16 +0700

    Add files via upload
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

---
+++ TUD_HK3_2627/database.js
@@ -0,0 +1,34 @@
+// database.js
+var DATA_COURSES = [
+    {
+        name: "Sinh hoạt công dân",
+        location: "Cơ sở: An Dương Vương",
+       room: "Hội trường B, Tầng 5, Dãy nhà B", link: "",
+	teacher: "",
+        time: "7:30-11:30",
+        day: 5, 
+        start: 2,
+        length: 5,
+        weeks: [1]
+    },
+	
+];
+
+var DATA_DEADLINES = [
+    {
+        title: "Pháp luật đại cương",
+        duration: "Từ 15/09/2025 đến 05/11/2025",
+        tag: "VLE",
+        icon: "success", // tương ứng class: success, fire, alert, soft-blue...
+        emoji: "📖",
+        weeks: [1]
+    },
+   
+];
+var DATA_LOCATIONS = [
+    { name: "Cơ sở 1: An Dương Vương (280 An Dương Vương, phường Chợ Quán, TP.HCM)", color: "#e0f2fe", link: "https://maps.app.goo.gl/Q8dRKtTZcqGeuEmy5" },
+    { name: "Cơ sở 2: Lê Văn Sỹ (222 Lê Văn Sỹ, phường Nhiêu Lộc, TP.HCM)", color: "#e6f9ef", link: "https://maps.app.goo.gl/7zCgiMmscdPFfCFv5" },
+    { name: "Cơ sở KTX: Lạc Long Quân (351A Lạc Long Quân, phường Hòa Bình, TP.HCM)", color: "#fff3e0", link: "https://maps.app.goo.gl/oV1mXHYDuW44cGbN6" },
+    { name: "Công viên Lê Thị Riêng (875 Cách Mạng Tháng Tám, phường Hòa Hưng, TP.HCM)", color: "#fef9c3", link: "https://maps.app.goo.gl/K7GzwaEcJwSb9dwGA" },
+    { name: "Học online (Hệ thống VLE; Phòng học ảo TEAM, Google Meet, Zoom)", color: "#f3e8ff", link: "#" }
+];
