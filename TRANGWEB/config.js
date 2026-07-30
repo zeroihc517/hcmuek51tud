6b8ac036596d65026a086bfc8ecdaf183a62defe
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-07-30 08:50:29 +0700

    Update config.js
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- TRANGWEB/config.js
+++ TRANGWEB/config.js
@@ -1,4 +1,4 @@
- const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwUnYQNtci8PL1kqE28AZq4qHnCRK8bHRViMW2WRGQMT9sA83GzQ7W2XzbrhjECe3DN/exec'; 
+ const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx-5bClHrCln4aYc1GtST9-kOFrEtU6UGTRf7-aaU6lNWSnH4exCjhmDq5XhfK7TMuX/exec'; 
 
      let isAdmin = localStorage.getItem('isAdmin') === 'true';
         let currentSheetName = "";
