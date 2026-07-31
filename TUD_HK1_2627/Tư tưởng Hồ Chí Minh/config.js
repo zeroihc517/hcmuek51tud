61646f175afce392b4136cfb954db611060cc3f3
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-07-31 20:16:22 +0700

    Update config.js
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- TUD_HK1_2627/Tư tưởng Hồ Chí Minh/config.js
+++ TUD_HK1_2627/Tư tưởng Hồ Chí Minh/config.js
@@ -1,3 +1,3 @@
 // CONFIG DUY NHẤT CHO CẢ HỆ THỐNG
 // Sau này đổi link API chỉ cần đổi 1 LẦN duy nhất ở đây!
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyerffs1u6zHF4GkYkNamIffUYEvlry-NbpCAr-KANe2R6xEaKI1avvKGwGpuReENQa0g/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxaBz8CW1ZHF1yrI6FDg15CaImaj_D4-GP_Qv35ET8nqhSN9YxBks0wuaIcwDaLAMFjyQ/exec";
