f2411ced0bcc5c1d2f05d90e37cba631bd20acdd
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-07-31 19:52:48 +0700

    Add files via upload
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- TUD_HK1_2627/Tư tưởng Hồ Chí Minh/config.js
+++ TUD_HK1_2627/Tư tưởng Hồ Chí Minh/config.js
@@ -1,3 +1,3 @@
 // CONFIG DUY NHẤT CHO CẢ HỆ THỐNG
 // Sau này đổi link API chỉ cần đổi 1 LẦN duy nhất ở đây!
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxCMAfTQPPNQIvz5R0IKEx1FGlwiGqTonCKoV3Q5Ed4j3eGaDD4RgYamRMU9LKRtIvPQA/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyerffs1u6zHF4GkYkNamIffUYEvlry-NbpCAr-KANe2R6xEaKI1avvKGwGpuReENQa0g/exec";
