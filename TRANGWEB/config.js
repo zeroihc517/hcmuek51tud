b0a804d243518c46ed1e473c273e088b525734e1
ZeroIHC <188943239+zeroihc517@users.noreply.github.com>
2026-09-23 14:00:25 +0700

    Add files via upload
    Signed-off-by: ZeroIHC <188943239+zeroihc517@users.noreply.github.com>

--- TRANGWEB/config.js
+++ TRANGWEB/config.js
@@ -1,4 +1,4 @@
- const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbye8MUdpgJQLUM-v1SnDciTdgmJVwmxQwVtzE0RiEtFfsi0duGxKljVUI0-TglTiZ_S/exec'; 
+ const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzKwFUgHPxPaf8n_DK_0ylPczzaXpP57Ry3Kp1ja-R93k7DTI1z9mKPQhAa7ZzY-hV2/exec'; 
 
      let isAdmin = false;
         let currentSheetName = "";
